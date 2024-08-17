02bcdfe71ef7ef4ddb79d2ecdf7c333fca93a539
fazil108 <onlinksolutionsinfo@gmail.com>
2024-08-17 22:28:04 +0530

    fix responsive bugs in contact us page

--- app/contact-us/page.tsx
+++ app/contact-us/page.tsx
@@ -40,45 +40,44 @@ const page = () => {
 
       <div className="py-20">
         <h3 className="text-center font-secondFont text-2xl lg:text-5xl">Send Us a Message</h3>
-        <div className="main_container mt-5">
-          <form className="flex justify-center" onSubmit={handleSubmit}>
-            <div className="bg-slate-100 w-full px-5 py-6 grid lg:w-2/3">
-              <div className="grid gap-3 md:grid-cols-2">
-                <div className="flex flex-col gap-2">
-                  <label>First Name:</label>
-                  <input type="text" name="fname" className="py-3 px-1 outline-yellow" placeholder="Enter your first name" required />
-                </div>
-                <div className="flex flex-col gap-2">
-                  <label>Last Name:</label>
-                  <input type="text" name="lname" className="py-3 px-1 outline-yellow" placeholder="Enter your last name" required />
-                </div>
-              </div>
 
-              <div className="flex flex-col gap-2 mt-3">
-                <label>Email:</label>
-                <input type="text" name="email" className="py-3 px-1 outline-yellow" placeholder="Enter your email" required />
+        <div className='main_container bg-slate-100 shadow-xl shadow-slate-300 py-10 mt-6 lg:flex lg:w-2/3 lg:justify-center'>
+          <form className='px-3 py-4 lg:w-3/4' onSubmit={handleSubmit}>
+            <div className='md:grid md:grid-cols-2 gap-2 lg:gap-5'>
+              <div className='flex flex-col'>
+                <label>First Name:</label>
+                <input type="text" name="fname" className="py-3 px-3 outline-yellow" placeholder="Enter your first name" required />
               </div>
-
-              <div className="flex flex-col gap-2 mt-3">
-                <label>Subject:</label>
-                <input type="text" name="subject" className="py-3 px-1 outline-yellow" placeholder="Enter your subject" required />
+              <div className='flex flex-col'>
+                <label>Last Name:</label>
+                <input type="text" name="lname" className="py-3 px-3 outline-yellow" placeholder="Enter your last name" required />
               </div>
-
-              <div className="flex flex-col gap-2 mt-3 outline-yellow">
-                <label>Message:</label>
-                <textarea rows={10} name="message" className="py-3 px-1 outline-yellow" placeholder="Enter your message" required />
-              </div>
-
-              <div className='flex w-full justify-end'>
-                <button type="submit" className="bg-yellow hover:text-white hover:bg-black duration-300 transition-all text-black py-4 px-5 self-center mt-5">Submit</button>
-              </div>
-
             </div>
-          </form>
-        </div>
-      </div>
 
-    </section>
+            <div className='flex flex-col mt-2'>
+              <label>Email:</label>
+              <input type="text" name="email" className="py-3 px-3 outline-yellow" placeholder="Enter your email" required />
+            </div>
+
+            <div className='flex flex-col mt-2'>
+              <label>Subject:</label>
+              <input type="text" name="subject" className="py-3 px-3 outline-yellow" placeholder="Enter your subject" required />
+            </div>
+
+            <div className='flex flex-col mt-2'>
+              <label>Message:</label>
+              <textarea rows={10} name="message" className="py-3 px-3 outline-yellow" placeholder="Enter your message" required />
+            </div>
+
+            <div className='flex w-full justify-center md:justify-end'>
+              <button type="submit" className="bg-yellow hover:text-white hover:bg-black duration-300 transition-all text-black py-4 px-5 self-center mt-5">Submit</button>
+            </div>
+
+          </form>
+        </div >
+      </div >
+
+    </section >
   )
 }
 
