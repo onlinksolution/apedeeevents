a063dabef068b106379bf026932c5ca7de337d58
fazil108 <onlinksolutionsinfo@gmail.com>
2024-08-18 18:46:24 +0530

    Final changes done

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,10 +1,8 @@
 import type { Metadata } from "next";
-import { Inter } from "next/font/google";
 import "./globals.css";
 import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
 import Loading from "./components/Loading";
-import { useEffect, useState } from "react";
 
 export const metadata: Metadata = {
   title: "Ape Dee Events UK LTD",
